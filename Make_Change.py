1d571f907365b42f50975e96c1ffe69b69e36f00
EddieMichael1983 <46540863+EddieMichael1983@users.noreply.github.com>
2019-03-27 21:16:12 -0700

    Update Make_Change.py

--- Make_Change.py
+++ Make_Change.py
@@ -10,10 +10,9 @@ dimes = unconverted_pennies // 10
 unconverted_pennies = unconverted_pennies % 10
 nickels = unconverted_pennies // 5
 unconverted_pennies = unconverted_pennies % 5
-pennies = unconverted_pennies // 1
-unconverted_pennies = unconverted_pennies % 1
+
 
 print(f'{quarters} quarters')
 print(f'{dimes} dimes')
 print(f'{nickels} nickels')
-print(f'{pennies} pennies')
+print(f'{unconverted_pennies} pennies')
